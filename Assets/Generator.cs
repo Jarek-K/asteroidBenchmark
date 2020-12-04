5ced4b8f8afc63f37f600906b51c5d41caffeb1c
Jarek-K <kuszcz95@gmail.com>
2020-12-04 14:54:14 +0100

    general idea

--- Assets/Generator.cs
+++ Assets/Generator.cs
@@ -7,7 +7,10 @@ public class Generator : MonoBehaviour
     public int asteroidRes;
     public GameObject asteroid;
     public float asteroidMaxV;
-    // Start is called before the first frame update
+    public float asteroidInstantiationTime;
+    public List<Asteroid> asteroids = new List<Asteroid>();
+    public List<Asteroid> bullets = new List<Asteroid>();
+    Asteroid player;
     void Start()
     {
         Random.InitState(42);
@@ -15,15 +18,65 @@ public class Generator : MonoBehaviour
         {
             for (int j = 0; j < asteroidRes; j++)
             {
-                GameObject tmpAsteroid = Object.Instantiate(asteroid, new Vector3(i, j, 0), Quaternion.identity);
-                tmpAsteroid.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-asteroidMaxV, asteroidMaxV), Random.Range(-asteroidMaxV, asteroidMaxV));
+
+                asteroids.Add(new Asteroid(i, j, new Vector2(Random.Range(-asteroidMaxV, asteroidMaxV), Random.Range(-asteroidMaxV, asteroidMaxV)), asteroidInstantiationTime));
             }
         }
     }
 
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        //start with movement
+
+        //check collisions
+        UpdateObject(player);
+
+        foreach (Asteroid aster in asteroids)
+        {
+            UpdateObject(aster);
+
+        }
+        foreach (Asteroid bullet in bullets)
+        {
+            UpdateObject(bullet);
+        }
+
+        //sort list
+    }
+
+    void UpdateObject(Asteroid obj)
+    {
+        obj.xPosition = obj.xPosition + obj.velocity.x;
+        obj.yPosition = obj.yPosition + obj.velocity.y;
+        //check collision
+
+    }
+}
+
+
+
+
+//maybe rename to collideable object or smth
+public class Asteroid
+{
+    public float xPosition;
+
+    public float yPosition;
+    public bool instantiate;
+    public float tillInstantiation;
+    public bool simulated;
+    public Vector2 velocity;
+
+    public Asteroid(float xPos, float yPos, Vector2 v, float intantiationTime)
+    {
+        xPosition = xPos;
+        yPosition = yPos;
+        velocity = v;
+        instantiate = false;
+        tillInstantiation = intantiationTime;
+        simulated = false;
 
     }
 }
