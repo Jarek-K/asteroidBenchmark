d1a6892639c9a15cf142212f596731ee4b9ea82c
Jarek-K <kuszcz95@gmail.com>
2020-12-04 14:26:28 +0100

    unity physics test

---
+++ Assets/Generator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public int asteroidRes;
+    public GameObject asteroid;
+    public float asteroidMaxV;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Random.InitState(42);
+        for (int i = 0; i < asteroidRes; i++)
+        {
+            for (int j = 0; j < asteroidRes; j++)
+            {
+                GameObject tmpAsteroid = Object.Instantiate(asteroid, new Vector3(i, j, 0), Quaternion.identity);
+                tmpAsteroid.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-asteroidMaxV, asteroidMaxV), Random.Range(-asteroidMaxV, asteroidMaxV));
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
